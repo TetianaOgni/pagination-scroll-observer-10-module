ad8993d6fa006403d99aac6d249a687d208d2869
TetianaOgni <119048580+TetianaOgni@users.noreply.github.com>
2023-06-02 21:53:03 +0000

    Deploying to gh-pages from @ TetianaOgni/pagination-scroll-observer-10-module@d86798458001784f8d885fc69dba6764b0430a70 🚀

---
+++ 01-authorization-bearer.d83adf96.js
@@ -0,0 +1,2 @@
+!function(){const e=new URLSearchParams({limit:30,page:1});fetch(`https://the-one-api.dev/v2/character?${e}`,{method:"GET",headers:{Authorization:"Bearer opvOoJ4b9dtIe0IanqJU"}}).then((e=>console.log(e)))}();
+//# sourceMappingURL=01-authorization-bearer.d83adf96.js.map
---
+++ 01-authorization-bearer.e77584dc.js
@@ -0,0 +1,2 @@
+!function(){var t,e;t=new URLSearchParams({limit:30,page:1}),e={method:"GET",headers:{Authorization:"Bearer ".concat("opvOoJ4b9dtIe0IanqJU")}},fetch("".concat("https://the-one-api.dev/v2/").concat("character","?").concat(t),e).then((function(t){return console.log(t)}))}();
+//# sourceMappingURL=01-authorization-bearer.e77584dc.js.map
